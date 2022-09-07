40aca7c81af8b5966da320ba7af756d5cbbf3bc6
unknown <brayanramirez45546@gmail.com>
2022-09-07 10:54:46 -0500

    avance de login

--- proyectoSena/backend/login.php
+++ proyectoSena/backend/login.php
@@ -1,8 +1,38 @@
 <?php
-    echo $_POST ["correo"];
-    echo "<br>";
-?>
-<?php
-    echo $_POST ["password"];
-    echo "<br>";
+  if(isset($_POST)){
+    /* si existe post se hace la conexion ala base de datos*/
+    include_once "/wamp64/www/exchangecity/proyectoSena/funciones/conexionDB.php";
+    /* se crean variables para poder validar los datos  */
+    $correo=  isset( $_POST["correo"]) ? $_POST["correo"]:false;
+     $contraseña = isset($_POST["password"]) ? $_POST["password"]:false;
+
+    /* se  crea un array para guardar los errores*/
+    $error=array();
+    
+   
+    $consulta_email="SELECT * FROM usuario WHERE usu_correo='$correo'";
+     $guardar_email = mysqli_query($db,$consulta_email);
+     $email_existente=mysqli_fetch_assoc($guardar_email);
+
+    
+     
+    
+
+
+
+     
+     
+
+   
+    //var_dump($email_exitente);
+
+   if(isset($email_existente)){
+        echo "existe";
+             
+    }else{
+        echo "no existe";
+   }
+
+}
+  
 ?>
