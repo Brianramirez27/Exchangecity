39cbb801ff3e4b5e1adaa95a812169feb4b3866a
AlejandroSantanilla <111874768+AlejandroSantanilla@users.noreply.github.com>
2022-09-19 13:01:15 -0500

    investigacion de mostrar imagen (#6)
    * investigacion de mostrar imagen
    
    * cmabios en imagen de login y registro
    
    * estilos
    
    Co-authored-by: unknown <santanillabrayan56@mal.com>

--- proyectoSena/backend/datosActualizar.php
+++ proyectoSena/backend/datosActualizar.php
@@ -1,10 +1,22 @@
 <?php
-    echo $_POST   ["nombres"];
-    echo $_POST   ["nombres"];
-    echo $_POST   ["nombres"];
-    echo $_POST   ["nombres"];
-    echo $_POST   ["nombres"];
+    include_once("/wamp64/www/Exchangecity/proyectoSena/funciones/conexionDB.php");
+
+    // echo $_POST["nombre"];
+    // echo $_POST["imagenPerfil"];
+    // echo $_POST["descripcion"];
+    // echo $_POST["apellido"];
+    // echo $_POST["correo"];
+    // echo $_POST["documento"];
+    // echo $_POST["ciudad"];
+    // echo $_POST["contacto"];
+    // echo $_POST["redSocial"];
+
+    echo $nombre= $_FILES["imagenPerfil"]["name"];
+    echo $tipo= $_FILES["imagenPerfil"]["type"];
+    echo  $tamaño=$_FILES["imagenPerfil"]["size"];
+    $carpeta= '/wamp64/www/Exchangecity/proyectoSena/imagenes/publicaciones/';
+    move_uploaded_file($_FILES["imagenPerfil"]["tmp_name"] ,$carpeta . $nombre);
     
-    echo "<br>";
+ 
 ?>
 
