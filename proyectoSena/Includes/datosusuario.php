3ca51a18999f639b2f90fd9c24ec87b2cfba9e5e
unknown <santanillabrayan56@mal.com>
2022-09-19 11:25:00 -0500

    investigacion de mostrar imagen

--- proyectoSena/Includes/datosusuario.php
+++ proyectoSena/Includes/datosusuario.php
@@ -10,26 +10,28 @@ include_once "/wamp64/www/exchangecity/proyectoSena/funciones/masFunciones.php";
     <div class="titulo">
         <h1>Perfil de los datos</h1>
     </div>
-    <section>
-    <form action="">
+    <section >
+    <form class="formDatos"  action="/exchangecity/proyectoSena/backend/datosActualizar.php"  enctype="multipart/form-data" method="POST">
       <div class="datosConjunto">
           <div class="datos">
               <div class="datoImagen">
-                  <img src="/exchangecity/proyectoSena/imagenes/conteiner/ciudad.jpg" alt="foto"> 
-                  <input type="file" placeholder="Foto de perfil">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed adipisci veritatis rerum consequuntur quibusdam magnam quaerat dolore possimus ex nihil architecto, corporis, labore molestias odio fugiat itaque rem, voluptas sit.</p>
-              </div>
+                  <img src="/exchangecity/proyectoSena/imagenes/conteiner/ciudad.jpg" alt="foto" > 
+                  <input type="file" placeholder="Foto de perfil" name="imagenPerfil">
+                  <textarea class="datosDes" name="descripcion" id="" cols="30" rows="10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta placeat hic, natus eius,
+                     sint amet esse repellat officiis facilis odit laudantium sed possimus! Minima asperiores voluptatum odio ullam! Minima, sint.</textarea>
+                  
+              </div>      
               <div class="datosGenerales">  
                 <div class="datosPersonales">
                   <h1>Datos personales</h1>
-                  <label class="labelDatos datosPreguntas">Nombres:</label>
-                  <input class="inputDatos inputLogin"  value="<?php echo $_SESSION["login_correcto"]["usu_nombre"] ?>"></input>
-                  <label class="labelDatos  datosPreguntas">Apellidos:</label>
-                  <input class="inputDatos inputLogin" value="<?php echo $_SESSION["login_correcto"]["usu_apellido"] ?>"></input>
-                  <label class="labelDatos  datosPreguntas">Correo Electronico</label>
-                  <input class="inputDatos inputLogin" value="<?php echo $_SESSION["login_correcto"]["usu_correo"] ?>"></input>
-                  <label class="labelDatos  datosPreguntas">Documento</label>
-                  <input class="inputDatos inputLogin"  value="<?php echo $_SESSION["login_correcto"]["usu_documento"] ?>"></input>
+                  <label class="labelDatos datosPreguntas" for="nombre">Nombres:</label>
+                  <input class="inputDatos inputLogin" name="nombre"  value="<?php echo $_SESSION["login_correcto"]["usu_nombre"] ?>"></input>
+                  <label class="labelDatos  datosPreguntas" for="apellido">Apellidos:</label>
+                  <input class="inputDatos inputLogin" name="apellido" value="<?php echo $_SESSION["login_correcto"]["usu_apellido"] ?>"></input>
+                  <label class="labelDatos  datosPreguntas" for="correo">Correo Electronico</label>
+                  <input class="inputDatos inputLogin" name="correo" value="<?php echo $_SESSION["login_correcto"]["usu_correo"] ?>"></input>
+                  <label class="labelDatos  datosPreguntas" for="documento">Documento</label>
+                  <input class="inputDatos inputLogin" name="documento" value="<?php echo $_SESSION["login_correcto"]["usu_documento"] ?>"></input>
 
                 </div>  
                 <div class="datosPagina">
@@ -41,10 +43,10 @@ include_once "/wamp64/www/exchangecity/proyectoSena/funciones/masFunciones.php";
                 </div>
                 <div class="datosContacto">
                   <h1>Datos contacto</h1>
-                  <label class="labelDatos  datosPreguntas" >Ubicacion</label>
+                  <label class="labelDatos  datosPreguntas" for="ciudad">Ubicacion</label>
                   <!-- funcion creada para consultar la ciudades y sus opciones -->
                   <?php $ciudades=ConsultarCiudades($db); ?> 
-                  <select class="inputDatos inputLogin"  value="<?php $_SESSION["login_correcto"]["FK_ciu_codigo_uc"]?>" >   
+                  <select class="inputDatos inputLogin" name="ciudad" value="<?php $_SESSION["login_correcto"]["FK_ciu_codigo_uc"]?>" >   
                   <?php while($ciudad=mysqli_fetch_assoc($ciudades)): ?>
                     <?php if($_SESSION["login_correcto"]["FK_ciu_codigo_uc"]==$ciudad["ciu_codigo"]):?>
                             <option selected="selected"  value="<?php $_SESSION["login_correcto"]["FK_ciu_codigo_uc"]?>"><?php echo  $ciudad["ciu_nombre"] ?></option>
@@ -54,10 +56,10 @@ include_once "/wamp64/www/exchangecity/proyectoSena/funciones/masFunciones.php";
                   <?php endwhile;?>               
                   </select>
 
-                  <label class="labelDatos  datosPreguntas">whatsapp</label>  
-                  <input class="inputDatos inputLogin" type="text" value="<?php echo $_SESSION["login_correcto"]["usu_contacto"] ?>"> 
-                  <label class="labelDatos  datosPreguntas">red social</label> 
-                  <input class="inputDatos inputLogin" type="text" value="<?php echo $_SESSION["login_correcto"]["usu_red_social"] ?>">
+                  <label class="labelDatos  datosPreguntas" name="contacto" >whatsapp</label>  
+                  <input class="inputDatos inputLogin" type="text" name="contacto" value="<?php echo $_SESSION["login_correcto"]["usu_contacto"] ?>"> 
+                  <label class="labelDatos  datosPreguntas" for="redSocial" >red social</label> 
+                  <input class="inputDatos inputLogin" type="text" name="redSocial" value="<?php echo $_SESSION["login_correcto"]["usu_red_social"] ?>">
                   
                 </div>
                 <div class="actualizarDatos">
