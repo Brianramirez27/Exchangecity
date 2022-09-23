6c505e9cbe4b62fb8f26c37c7c849c067219f907
AlejandroSantanilla <111874768+AlejandroSantanilla@users.noreply.github.com>
2022-09-23 08:53:08 -0500

    se agregan includes en descripcion (#10)
    Co-authored-by: unknown <santanillabrayan56@mal.com>

---
+++ proyectoSena/Includes/categorias.php
@@ -0,0 +1,21 @@
+<?php 
+include_once "/wamp64/www/Exchangecity/proyectoSena/funciones/conexionDB.php";
+include_once "/wamp64/www/exchangecity/proyectoSena/Includes/header.php";
+include_once "/wamp64/www/exchangecity/proyectoSena/Includes/menulateral.php";
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+  include_once "/wamp64/www/exchangecity/proyectoSena/Includes/footer.php"
+  
+  ?>
--- proyectoSena/Includes/menulateral.php
+++ proyectoSena/Includes/menulateral.php
@@ -7,25 +7,25 @@
       </a>
     </div>
     <div class="icono">
-      <a class="menuObjeto" href="televisores.php">
+      <a class="menuObjeto" href="/exchangecity/proyectoSena/Includes/categorias.php">
         <img class="menuIcono" src="/exchangecity/PROYECTOSENA/imagenes/Navegacion/svg/tv.svg" alt="televisores.svg">
         <span>TV</span>
       </a>
     </div>
     <div class="icono">
-      <a class="menuObjeto" href="movil.php">
+      <a class="menuObjeto" href="/exchangecity/proyectoSena/Includes/categorias.php">
         <img class="menuIcono" src="/exchangecity//PROYECTOSENA/imagenes/Navegacion/svg/smartphone.svg" alt="movil.svg">
         <span>Movil</span>
       </a>
     </div>
     <div class="icono">
-      <a class="menuObjeto" href="computadoras.php">
+      <a class="menuObjeto" href="/exchangecity/proyectoSena/Includes/categorias.php">
         <img class="menuIcono" src="/exchangecity//PROYECTOSENA/imagenes/Navegacion/svg/pc.svg" alt="computadoras.svg">
         <span>PC</span>
       </a>
     </div>
     <div class="icono">
-      <a class="menuObjeto" href="consolas.php">
+      <a class="menuObjeto" href="/exchangecity/proyectoSena/Includes/categorias.php">
         <img class="menuIcono" src="/exchangecity//PROYECTOSENA/imagenes/Navegacion/svg/console-controller.svg" alt="consolas.svg">
         <span> Consolas</span>
       </a>
