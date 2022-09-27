0f35633931acdb96f04d1448e893a7b9a9da9c71
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-09-27 17:04:53 -0500

    Cod 25 (#17)
    * avances de editar publicacion
    
    * se termino la visualizacion de la publicacion quedo pendiente el eliminar publicacion

--- proyectoSena/Includes/editarPublicacion.php
+++ proyectoSena/Includes/editarPublicacion.php
@@ -5,91 +5,97 @@ include_once "/wamp64/www/exchangecity/proyectoSena/Includes/menulateral.php";
 include_once "/wamp64/www/Exchangecity/proyectoSena/funciones/masFunciones.php";
 
 ?>
-
-  
+ 
 
  <!--contenido de la pagina o aside-->
   <section id="container">
-  <div class="titulo">
-            <h1>Editar Publicaciones</h1>
-        </div>
+    <div class="titulo">
+      <h1>Editar Publicacion</h1>
+    </div>
     <section>
-        <?php  $datosPublicacion=ConsultarPublicacionesUsuario($db,$_SESSION["login_correcto"]["usu_codigo"]); ?>
-        <?php  while($publicacionUsuario=mysqli_fetch_assoc($datosPublicacion)):?>
-            <?php $categoriaPublicacion=ConsultarCategoriaPublicacion($db,$publicacionUsuario["FK_cat_codigo_pc"]) ;
-                $nombreCategoria=mysqli_fetch_assoc($categoriaPublicacion); 
-            ?>
-                <div class="carsBottom">
-                    <div class="cars">
-                        <h3><?php echo $nombreCategoria["cat_tipo"];?></h3>
-                        <img src="data:image/JPG;base64,<?php echo base64_encode($publicacionUsuario["pub_img_general"]);?>" />
-                        <div class="conterCars">
-                        <a href="/PROYECTOSENA/paginas/descricion.html"><h2>producto</h2></a>
-                        <p><?php echo substr($publicacionUsuario["pub_descripcion"],0,30)."...";?></p>
-                        <?php  $estadoPublicacion=ConsultarEstadoPublicacion($db,$publicacionUsuario["FK_est_codigo_pe"]);
-                            $nombreEstado=mysqli_fetch_assoc($estadoPublicacion);
-                        ?>
-                        <p class="estado"><?php echo $nombreEstado["est_nombre"];?></p>
-                        </div>
-                    </div>
-                    <button class="editar">Editar</button>
-                </div>
-         
-                <form class="editarPublicacion  publicarProductos" action="/PROYECTOSENA/backend/publicacionEditar.php" method="POST">
-                    <div class="tituloPublicar"> 
-                        <h1>Actualiazar Producto</h1>
-                        <p class="cerrarPublicarProductos">x</p>
-                    </div>
-                    <label for="tituloPublicacionv">Titulo Publicacion</label>
-                <?php echo isset ($_SESSION["noVerificado"]) ? $_SESSION["noVerificado"]:""; ?>
-                <input  class="inputPublicar inputLogin" type="text" name="tituloPublicacion">
+        <?php $detallesPublicacion=ConsultarDescripcionPublicacion($db,$_REQUEST["pub_codigo"]);
+            $infoPublicacion=mysqli_fetch_assoc($detallesPublicacion);
+        ?>
+    <form class="publicarProductos" action="/exchangecity/PROYECTOSENA/backend/publicacionEditar.php?pub_codigo=<?php echo $_REQUEST["pub_codigo"];?>" enctype="multipart/form-data" method="POST">
+                <label for="tituloPublicacionv">titulo</label>
+                <input  class="inputPublicar inputLogin" type="text" name="tituloPublicacion" value="<?php echo $infoPublicacion["pub_titulo"] ?>">
                 <label for="descripcion">Descripcion de la Publicacion</label>
-                <textarea class="inputPublicar inputLogin" name="descripcion"></textarea>
+                <textarea class="inputPublicar inputLogin" name="descripcion" ><?php echo $infoPublicacion["pub_descripcion"]?></textarea>
                 <label for="intereses">intereses de intercambio</label>
-                <textarea class="inputPublicar inputLogin" name="intereses"></textarea>
+                <textarea class="inputPublicar inputLogin" name="intereses"><?php echo $infoPublicacion["pub_intereses"]?></textarea>
                 <?php $categorias=ConsultarCategoria($db) ?>
                 <label for="categoriaId">Categorias</label>
-                <?php echo isset ($_SESSION["error_Categoria"]) ? $_SESSION["error_Categoria"]:""; ?>
+
+                <?php $categorias_editar= ConsultarCategoria($db);?>
                 <select name="categoriaId">
-                    <option selected="selected" disabled="diseabled" >Seleccionar</option>
-                  <?php while($categoria=mysqli_fetch_assoc($categorias)): ?>                   
-                    <option><?php echo $categoria["cat_tipo"];?></option>
-                    
-                    <?php endwhile;?>   
+                <?php while($categoria=mysqli_fetch_assoc($categorias_editar)) :?>
+                    <?php if($categoria["cat_codigo"]==$infoPublicacion["FK_cat_codigo_pc"]):?>
+                            <option selected="selected"  value="<?php $categoria["cat_tipo"]?>"><?php echo $categoria["cat_tipo"] ?></option>
+                            <?php else:?>
+                            <option ><?php echo $categoria["cat_tipo"]?></option> 
+                    <?php endif; ?>
+                <?php endwhile;?>
                 </select>
+
+            
                 <label for="estado"> Estado del producto</label>
-                <?php echo isset ($_SESSION["error_Estado"]) ? $_SESSION["error_Estado"]:""; ?>
-                <?php $estados=ConsultarEstado($db);
+                <?php $estados_editar=ConsultarEstado($db);
                  ?>
+
                 <select name="estado">
-                <option selected="selected" disabled="diseabled" >Seleccionar</option>
-                    <?php while($estado=mysqli_fetch_assoc($estados)):?>
-                      <option><?php echo $estado["est_nombre"];?></option> 
-                    <?php endwhile; ?>
+                  <?php while($estado=mysqli_fetch_assoc($estados_editar)):?>
+                    <?php if($estado["est_codigo"]==$infoPublicacion["FK_est_codigo_pe"]):?>
+                            <option selected="selected"  value="<?php echo $estado["est_nombre"]?>"><?php echo $estado["est_nombre"] ?></option>
+                            <?php else:?>
+                            <option ><?php echo $estado["est_nombre"]?></option> 
+                    <?php endif; ?>
+                <?php endwhile;?>
                 </select>
-                <label for="fotoPrincipal">Foto Principal</label>
-                <?php echo isset ($_SESSION["errorImagen"]) ? $_SESSION["errorImagen"]:""; ?>
-                <input class="inputPublicar inputLogin" type="file" name="fotoPrincipal">
-                <label  class="label" for="imagen1">Imagen 1</label>
-                <input class="inputPublicar inputLogin" type="file" id="imagen1">
-                <label for="imagen2">Imagen 2</label>
-                <input class="inputPublicar inputLogin" type="file" name="imagen2">
-                <label for="imagen3">Imagen 3</label>
-                <input class="inputPublicar inputLogin" type="file" name="imagen3">
-                <label for="imagen4">Imagen 4</label>
-                <input class="inputPublicar inputLogin" type="file" name="imagen4">
-                <input  class="buttomProducto actualizarDatos" type="submit" value="Publicar">
-                </form>
-        <?php endwhile; ?> 
-        <script src="/exchangecity/PROYECTOSENA/js/editarPublicacion.js"></script>
-        <?php include_once "/wamp64/www/Exchangecity/proyectoSena/Includes/acount.php" ?>
+                <div class="conten_img_editar">
+                <div class="archivo_img">
+                    <label class="label"  for="fotoPrincipal">Foto Principal</label>
+                    <input class="img inputPublicar inputLogin" type="file" id="fotoPrincipal">
+                    <img class="img_editar" src="data:image/JPG;base64,<?php echo base64_encode($infoPublicacion["pub_img_general"]);?>" />
+                </div>
+                <div class="archivo_img">
+                    <label  class="label" for="imagen1">Imagen 1</label>
+                    <input class="img inputPublicar inputLogin" type="file" id="imagen1">
+                    <img class="img_editar" src="data:image/JPG;base64,<?php echo base64_encode($infoPublicacion["pub_img1"]);?>" />
+                </div>
+                <div class="archivo_img">
+                    <label class="label"  for="imagen2">Imagen 2</label>
+                    <input class="img inputPublicar inputLogin" type="file" id="imagen2">
+                    <img class="img_editar" src="data:image/JPG;base64,<?php echo base64_encode($infoPublicacion["pub_img2"]);?>" />
+                </div>
+                <div class="archivo_img">
+                    <label class="label"  for="imagen3">Imagen 3</label>
+                    <input class="img inputPublicar inputLogin" type="file" id="imagen3">
+                    <img class="img_editar" src="data:image/JPG;base64,<?php echo base64_encode($infoPublicacion["pub_img3"]);?>" />
+                </div>
+                <div class="archivo_img">
+                    <label class="img label"  for="imagen4">Imagen 4</label>
+                    <input class="img inputPublicar inputLogin" type="file" id="imagen4">
+                    <img class="img_editar" src="data:image/JPG;base64,<?php echo base64_encode($infoPublicacion["pub_img4"]);?>" />
+                </div>
+            
+                </div>
+                <div class="enviar_eliminar">
+                <input  class="Eliminar" type="submit" value="Actualizar Publicacion">
+                <a class="Eliminar" href="#">Eliminar Publicacion</a></>
+                </div>
+            </form>
+  <?php include_once "/wamp64/www/Exchangecity/proyectoSena/Includes/acount.php"?>
     </section>
   </section>
+
+  
+
+
+
   <!--limpiamos los flotados del aside -->
   <div style="clear: both"></div>
 
  
   <?php 
   include_once "/wamp64/www/exchangecity/proyectoSena/Includes/footer.php"
-  
   ?>
