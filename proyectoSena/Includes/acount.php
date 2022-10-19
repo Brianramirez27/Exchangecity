34e9f2d148bba7d66d7e4ecafcea45dc5df75f25
Brayan Ramirez <Brayanramirez45456@gmail.com>
2022-10-19 10:45:34 -0500

    areglo de botones falta configurar

--- proyectoSena/Includes/acount.php
+++ proyectoSena/Includes/acount.php
@@ -1,4 +1,5 @@
 <?php 
+include_once "/wamp64/www/Exchangecity/proyectoSena/funciones/masFunciones.php";
 if(isset($_SESSION["usuario"])):?>
 <div class="acount">
       <a href="/exchangecity/proyectoSena/Includes/datosusuario.php">Datos Personales</a>
@@ -6,15 +7,20 @@ if(isset($_SESSION["usuario"])):?>
       <a href="/exchangecity/proyectoSena/Includes/misPublicaciones.php">Mis publicaciones</a>
       <?php $pubUsu= ConsultarPublicacionesUsuario($db,$_SESSION["login_correcto"]["usu_codigo"]);
         $usuPub=mysqli_fetch_assoc($pubUsu);
-        $intercambios_activos=IntercambioUsuarioProveedor($db,$_SESSION["login_correcto"]["usu_codigo"])   
+        $intercambios_activos=IntercambioUsuarioProveedor($db,$_SESSION["login_correcto"]["usu_codigo"]);
+        $tres=isset($intercambios_activos) ? $intercambios_activos:false;
+        $cuatro=isset($intercambios_activos["int_exitosa"])==1 ? $intercambios_activos["int_exitosa"]==1:false;
       ?>
-      <?php if($usuPub && $intercambios_activos["int_exitosa"]==1):?>
+
+      <?php if(isset($usuPub) &&  $tres==false || $cuatro==1):?>
+    
         <a href="/exchangecity/proyectoSena/Includes/intercambio/crearIntercambio.php">Intercambio</a>
+        
       <?php endif;?>
       
-      <?php if($intercambios_activos["int_exitosa"]==0):?>
-        <a href="/exchangecity/proyectoSena/Includes/intercambio/2Intercambio.php">Opciones Intercambio</a>
-      <?php endif;?>
+      <?php if($tres==false || $cuatro==false):?>
+          <a href="/exchangecity/proyectoSena/Includes/intercambio/2Intercambio.php">Opciones Intercambio</a>
+        <?php endif;?>
     </div>
 <script src="/exchangecity/PROYECTOSENA/js/acount.js"></script>
 <?php endif;?>
